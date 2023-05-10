7609824d370a144dc8fd173cc0971bd3c8023ed6
Marko Grdinić <mrakgr@gmail.com>
2023-05-10 11:25:32 +0200

    Prepping for message pack.

--- src/Client/Test.fs
+++ src/Client/Test.fs
@@ -4,25 +4,22 @@ open Fable.Bindings.SignalR
 open Fable.Bindings.SignalR.Exports
 open Thoth.Json
 
-printfn "%A" (Fable.Bindings.SignalR.Exports.Utils.VERSION)
+type W = Qwe of float | Asd of bool
+type R = {x : string; y : W}
 
-// printfn "%A" Fable.Bindings.SignalR.Protocols.
+let hub =
+    HubConnectionBuilder.Create()
+        .withUrl("socket/test")
+        .withAutomaticReconnect()
+        // .withHubProtocol(Protocol.MsgPack.Exports.MessagePackHubProtocol.Create())
+        .build()
 
-// type W = Qwe of float | Asd of bool
-// type R = {x : string; y : W}
-//
-// let hub =
-//     HubConnectionBuilder.Create()
-//         .withUrl("socket/test")
-//         .withAutomaticReconnect()
-//         .build()
-//
-// hub.on("response", fun (q : string) ->
-//     printfn "%A" (Decode.Auto.fromString<R> q)
-//     )
-//
-// promise {
-//     do! hub.start()
-//     do! hub.send("Hello", Encode.Auto.toString {x = "How are you today?"; y = Qwe 1.23})
-// } |> ignore
+hub.on("response", fun (q : string) ->
+    printfn "%A" (Decode.Auto.fromString<R> q)
+    )
+
+promise {
+    do! hub.start()
+    do! hub.send("Hello", Encode.Auto.toString {x = "How are you today?"; y = Qwe 1.23})
+} |> ignore
 
