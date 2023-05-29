937c8837104fa569a963a8860f920b9c873cd487
Marko Grdinić <mrakgr@gmail.com>
2023-05-29 17:03:31 +0200

    Here is the .NET 8 Preview 3 min repro. I'll try with .NET 7 next.

---
+++ Fun/Library.fs
@@ -0,0 +1,27 @@
+﻿namespace Fun
+
+open System.Threading
+open Microsoft.AspNetCore.Components
+
+type ViewComponent() =
+    inherit ComponentBase()
+    
+    let token_source = new CancellationTokenSource()
+    let cancellation_token = token_source.Token
+    
+    let mb init update =
+        let mb =
+            new MailboxProcessor<_>(fun mb -> async {
+                let mutable model = init
+                while true do
+                    let! msg = mb.Receive()
+                    model <- update msg model mb.Post
+                },cancellation_token) // Removing the cancellation token makes it work.
+        mb.Start()
+        mb
+        
+    let srv = mb () (fun x () _ -> printfn "%s" x)
+    
+    do srv.Post("Hello from view")
+    
+    member _.Say(x) = srv.Post x
--- UI/Program.cs
+++ UI/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Lproj.UI;
+using UI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
